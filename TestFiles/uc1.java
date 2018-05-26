55ef09d5a5b21791a56922e8b1c18fa221bf1052
Ammar Hasan <hasanammar94@gmail.com>
2018-05-26 15:09:07 +0200

    Add different usecases and config files

---
+++ TestFiles/uc1.java
@@ -0,0 +1,24 @@
+
+// Use Case 1 : Class with specific Access Modifiers
+
+public class Person {
+    String firstName;
+    String secondName;
+    Integer age;
+} 
+
+/* Following examples will result in error */
+
+/* Problem: wrong class name */
+//public class Person2 {
+    // String firstName;
+    // String secondName;
+    // Integer age;
+//} 
+
+/* Problem: wrong access modifier */
+//private class Person {
+    // String firstName;
+    // String secondName;
+    // Integer age;
+//} 
---
+++ TestFiles/uc4.java
@@ -0,0 +1,43 @@
+
+// Use Case 4 : Class with specific Method implementing 
+// simple mutlplication of two +ve number without using * or Math.multiplyFull method.
+// Method signature is given and call to other user defined method is restricted
+
+public class DemoMath {
+    static long mutliply(int a, int b) {
+      long result = 0;
+      for (int i=0; i < a; i++) {
+        result += b;
+      }
+      return b;
+    }
+} 
+
+
+/* Following examples will result in error */
+
+// Problems: static modifier, function signature, using forbidden operator, not using required construct
+// public class DemoMath {
+//     int mutliply(int a, double b) {
+//       return a*b;
+//     }
+// } 
+
+// Problems: using forbidden builtin method
+// public class DemoMath {
+//     static long mutliply(int a, int b) {
+//       for (int i=0; i < 2; i++) {}
+//       return Math.multiplyFull(a, b);
+//     }
+// } 
+
+// Problems: implementing in user defined method
+// public class DemoMath {
+//     static long mutliply(int a, int b) {
+//       for (int i=0; i < 2; i++) {}
+//       return MyMultiply(a, b);
+//     }
+//     static long MyMutliply(int a, int b) {
+//       return a*b;
+//     }
+// } 
